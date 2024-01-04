1a97022cba2061398c5af2ab08458feadd8bb695
Stephen Gaito <stephen@perceptisys.co.uk>
2024-01-04 17:00:10 +0000

    updated example report according to MFG suggestions

--- report.sql
+++ report.sql
@@ -1,14 +1,13 @@
-
-
 SELECT
  categories.description AS class,
  CONCAT('<a target="_blank" href="/cgi-bin/koha/members/moremember.pl?borrowernumber=',borrowernumber,'">',firstname, " ", surname,'</a>') AS borrower,
  GROUP_CONCAT('<a target="_blank" href="/cgi-bin/koha/catalogue/detail.pl?biblionumber=',biblionumber,'">',biblio.title,'</a>' SEPARATOR "</br>") AS titles,
- IF( MIN(date_due) <= CURDATE(),CONCAT('<span class="overdue">',MIN(date_due),'</span>'),MIN(date_due)) AS due
+ DATE_FORMAT(issuedate, '%Y %b %e') AS 'Date Issued',
+ DATEDIFF(CURDATE(), issuedate) DIV 7 AS 'Weeks Out',
+ IF( MIN(date_due) <= CURDATE(),CONCAT('<span class="overdue">',DATE_FORMAT(date_due, '%Y %b %e'),'</span>'),DATE_FORMAT(date_due, '%Y %b %e')) AS 'Date Due'
 FROM borrowers
 JOIN issues USING (borrowernumber)
 JOIN items USING (itemnumber)
 JOIN biblio USING (biblionumber)
 JOIN categories USING (categorycode)
-GROUP BY categorycode,firstname,surname
-
+GROUP BY categorycode,date_due,firstname,surname
